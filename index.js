dab1a6c1f275a0b62f8dfbeb9e79b506872a245d
thesebonesalone <duikhead2@gmail.com>
2020-11-04 13:59:28 -0600

    Pushing so Josh can work the problems

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, call) {
+      for (const item in collection) {
+        call(collection[item])
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, call) {
+      let new_array = []
+      for(let item in collection) {
+        new_array.push(call(collection[item]))
+        console.log(call(collection[item]))
+      }
+      return new_array
     },
 
     reduce: function() {
 
+
+
     },
 
     functions: function() {
+
+
 
     },
 
